2ee0609b9f9081118f12c62953c8a763663ae711
gneinaz123 <gneinazreen3@gmail.com>
2025-05-02 09:35:56 +0800

    test naz commit

--- KetoDietApp/ContentView.swift
+++ KetoDietApp/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world! hi dilum")
+            Text("Hello, world! hi check naz")
         }
         .padding()
     }
